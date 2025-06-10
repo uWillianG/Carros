af0be5d64589743d8a6587a9fd1689088f071329
Willian Siqueira <willian.gabriel.siq@gmail.com>
2025-06-09 23:12:55 -0300

    Update client.py

--- openai_api/client.py
+++ openai_api/client.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv, find_dotenv
 
 _ = load_dotenv(find_dotenv())
 
-client = openai.Client()
+client = OpenAi()
 
 def get_car_ai_bio(model, brand, year):
     message = ''''
